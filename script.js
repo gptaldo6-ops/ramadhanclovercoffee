a2a49d01842adbc3eab9f4b8ef46941361b0017b
gptaldo6-ops <gptaldo6@gmail.com>
2026-03-01 20:08:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -104,6 +104,8 @@ const ADD_ON_ITEMS = [
   { name: "Peach Tea", category: "Drink", price: 20000, image: "./img/addon/peach-tea.jpg" },
   { name: "Javakisa", category: "Drink", price: 20000, image: "./img/addon/javakisa.jpg" },
   { name: "Apple Rock", category: "Drink", price: 20000, image: "./img/addon/apple-rocl.jpg" },
+  { name: "Mie Nyemek, category: "Food", price: 23000, image: "./img/addon/mie.jpg" },
+  { name: "Bolognese", category: "Food", price: 30000, image: "./img/addon/bolognese.jpg" },
   { name: "Nasi", category: "Food", price: 5000, image: "./img/addon/rice.jpg" },
   { name: "Mix Platter", category: "Food", price: 25000, image: "./img/addon/mix-platter.jpg" },
   { name: "Kentang Goreng", category: "Food", price: 22000, image: "./img/addon/kentang-goreng.jpg" },
